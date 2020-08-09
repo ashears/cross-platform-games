3191edc284b4f0612d9d0a4042dd4a0650ba609d
Ashton Shears <shears.ash@gmail.com>
2020-08-09 09:42:39 -0700

    json2table js

---
+++ json2table.js
@@ -0,0 +1,34 @@
+function json2Table(json) {
+    let cols = Object.keys(json[0]);
+
+
+    //Map over columns, make headers,join into string
+    let headerRow = cols
+        .map(col => `<th>${col}</th>`)
+        .join("");
+
+    let rows = json.map(row => {
+        let tds = cols.map(col => {
+            if (row[col] == "")
+                return `<td>❎</td>`;
+            else if (!row[col].includes('http'))
+                return `<td>${row[col]}</td>`;
+            else
+                return `<td><a href="${row[col]}">✅</a></td>`;
+        }).join("");
+        return `<tr>${tds}</tr>`;
+    }).join("");
+
+    //build the table
+    const table = `
+        <table>
+            <thead>
+                <tr>${headerRow}</tr>
+            <thead>
+            <tbody>
+                ${rows}
+            <tbody>
+        <table>`;
+
+    return table;
+}
